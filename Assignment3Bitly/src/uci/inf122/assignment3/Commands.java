587a26ae0b3d6e45a3c064ca91483371f164101b
Manuel Yang <manuely@uci.edu>
2013-02-27 23:22:21 -0800

    Changed folder named from assignment 3 -> 4

--- Assignment3Bitly/src/uci/inf122/assignment3/Commands.java
+++
@@ -1,6 +0,0 @@
-package uci.inf122.assignment3;
-
-public enum Commands 
-{
-	LOGIN, LOGOUT, BITMARK, EXPAND, WATCH, UNWATCH, HOUR, WEEK
-}
--- Assignment3Bitly/src/uci/inf122/assignment3/commands/Command.java
+++
@@ -1,8 +0,0 @@
-package uci.inf122.assignment3.commands;
-
-import uci.inf122.assignment3.BitlyCommandHandler;
-
-public interface Command 
-{
-	String execute(BitlyCommandHandler bch);
-}
